83a442134e75e13680f3fb448ec9525e97c9d37b
Aymen Loudiy <nimuehilya@proton.me>
2023-09-27 18:42:58 +0100

    Display functional buttons and refactoring Add input buttons for color picking and grid layout Link buttons to their respective functions with event listeners Refactor grid generation function and add standard grid on page load and to reset button.

--- index.js
+++ index.js
@@ -1,47 +1,101 @@
+
+//Declaring all variables
+
+
 const container = document.querySelector('.container')
-console.log(container.style);
-let number = 10
-let gridNumber = number**2
-let R = 255
-let G = 255
-let B = 255
-let mode = 'rainbow'
-
-for (let index = 1; index <= gridNumber;  index++) {
-    let divLoop = document.createElement('div')
-    divLoop.classList.add('etch-node')
-    divLoop.style.width = `${(container.clientWidth-2*Math.sqrt(gridNumber))/Math.sqrt(gridNumber)}px`
-    divLoop.style.height = `${(container.clientHeight-2*Math.sqrt(gridNumber))/Math.sqrt(gridNumber)}px`
-    container.appendChild(divLoop)
-}
-const nodes = document.querySelectorAll('div.etch-node')
-
+const colorPicker = document.querySelector('.color-choice')
+const grid = document.querySelector('.grid-number')
+const colorInput = document.querySelector('.colorChoice')
+const standardMode = document.querySelector('.standard-toggle')
+const rainbowMode = document.querySelector('.rainbow-toggle')
+const gridReset = document.querySelector('.reset-grid')
+let nodes = document.querySelectorAll('div.etch-node')
 let mouseDown = false
-container.addEventListener('mousedown',()=>{
-    mouseDown = true
-})
-container.addEventListener('mouseup',()=>{
-    mouseDown = false
-})
+let R 
+let G 
+let B 
+let color = colorInput.value
+let mode = 'standard'
+
+
+//Generating standard 4x4 grid
+
+
+standardGrid()
+
+
+//functions
+
+
+
+//function to generate grid
+
+function loadGrid(gridNumber) {
+    container.innerHTML = ''
+    for (let index = 1; index <= gridNumber;  index++) {
+        let divLoop = document.createElement('div')
+        divLoop.classList.add('etch-node')
+        divLoop.style.width = `${(container.clientWidth-2*Math.sqrt(gridNumber))/Math.sqrt(gridNumber)}px`
+        divLoop.style.height = `${(container.clientHeight-2*Math.sqrt(gridNumber))/Math.sqrt(gridNumber)}px`
+        container.appendChild(divLoop)
+        nodes = document.querySelectorAll('div.etch-node')
+        nodes.forEach(node => {
+            node.addEventListener('mouseover',applyColor)
+            node.addEventListener('mousedown',applyColor)
+        })
+    }
+}
+
+//function to generate standard grid
+
+function standardGrid() {
+    loadGrid(16)
+    grid.value=''
+}
+
+
+//function to randomize colors
+
+function randomColors() {
+    R = Math.random()* 255
+    G = Math.random()* 255
+    B = Math.random()* 255
+}
+
+//function to apply mode depending on button selected
+
 function applyColor (e) {
     if (e.type === 'mouseover' && mouseDown == false) {
         return
     }
     else {
         if (mode == 'standard') {
-            e.target.style.backgroundColor = 'blue'
+            color = colorInput.value
+            e.target.style.backgroundColor = `${color}`
         }
         else if (mode == 'rainbow') {
             randomColors()
-            e.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`        }
+            e.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`        
+        }
     }
 }
-nodes.forEach(node => {
-    node.addEventListener('mouseover',applyColor)
-    node.addEventListener('mousedown',applyColor)
+
+
+//event listners
+
+container.addEventListener('mousedown',()=>{
+    mouseDown = true
 })
-function randomColors() {
-    R = Math.random()* 255
-    G = Math.random()* 255
-    B = Math.random()* 255
-}
+container.addEventListener('mouseup',()=>{
+    mouseDown = false
+})
+standardMode.addEventListener('click',()=> {
+    mode = 'standard'
+})
+rainbowMode.addEventListener('click',()=> {
+    mode = 'rainbow'
+})
+grid.addEventListener('change',()=>{
+    loadGrid(Number(grid.value)**2)
+})
+gridReset.addEventListener('click',standardGrid)
